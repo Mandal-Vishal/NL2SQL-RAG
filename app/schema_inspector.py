1a8d88d1b80dd4526df1ec56156734182352e93b
Vishal Mandal <mandalvishal978@gmail.com>
2026-08-21 13:47:31 +0530

    Add hget_columns and  get_foreign_keys func

--- app/schema_inspector.py
+++ app/schema_inspector.py
@@ -12,6 +12,7 @@ def get_connection():
     database = os.getenv('MYSQL_DATABASE')
 )
 
+#function to find tables in a database
 def get_tables():
     connection = get_connection()
     cursor = connection.cursor()
@@ -25,8 +26,44 @@ def get_tables():
 
     return tables
 
-if __name__ == '__main__':
-    tables = get_tables()
+#function to find columns in a table
+def get_columns(table_name):
+    connection = get_connection()
+    cursor = connection.cursor()
 
-    for table in tables:
-        print(table[0])
+    cursor.execute(f"describe {table_name}")
+
+    columns = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return columns
+
+#function to find foreign-key relationships of any table
+def get_foreign_keys(table_name):#
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    query = """
+    select column_name , referenced_table_name , referenced_column_name
+    from information_schema.key_column_usage
+    where table_schema = %s
+    and table_name = %s
+    and referenced_table_name is not null
+    """
+
+    cursor.execute(query , (os.getenv('MYSQL_DATABASE') , table_name))
+
+    foreign_keys = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return foreign_keys
+
+#For testing
+if __name__ == '__main__':
+    foreign_keys = get_foreign_keys("bookings")
+    for foreign_key in foreign_keys:
+        print(foreign_key)
